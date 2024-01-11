356bf48e13494b0aa21b486499942f721d925b40
SvenFrankson <sven.frankson@gmail.com>
2024-01-11 13:11:28 +0100

    Add base frame.

--- build/marble-fall.js
+++ build/marble-fall.js
@@ -350,11 +350,6 @@ class Game {
             });
             this.trackEditor = new TrackEditor(this);
             this.trackEditor.initialize();
-            let wall = BABYLON.MeshBuilder.CreatePlane("wall", { width: 2, height: 2, sideOrientation: BABYLON.Mesh.DOUBLESIDE, frontUVs: new BABYLON.Vector4(0, 0, 4, 4) });
-            wall.position.x += 0.6;
-            wall.position.y -= 0.8;
-            wall.position.z += 0.016;
-            wall.rotation.z = Math.PI / 2;
             let wallMaterial = new BABYLON.StandardMaterial("wood-material");
             wallMaterial.diffuseColor.copyFromFloats(0.2, 0.2, 0.2);
             wallMaterial.diffuseTexture = new BABYLON.Texture("./datas/textures/wood-color.jpg");
@@ -362,14 +357,52 @@ class Game {
             wallMaterial.specularTexture = new BABYLON.Texture("./datas/textures/wood-roughness.jpg");
             wallMaterial.specularColor.copyFromFloats(0.2, 0.2, 0.2);
             wallMaterial.bumpTexture = new BABYLON.Texture("./datas/textures/wood-normal-2.png");
-            wall.material = wallMaterial;
             let minX = Infinity;
             let maxX = -Infinity;
             let minY = Infinity;
             let maxY = -Infinity;
             for (let i = 0; i < this.tracks.length; i++) {
                 let track = this.tracks[i];
+                minX = Math.min(minX, track.position.x - tileWidth * 0.5);
+                maxX = Math.max(maxX, track.position.x + tileWidth * (track.deltaI + 0.5));
+                minY = Math.min(minY, track.position.y - tileHeight * (track.deltaJ + 1));
+                maxY = Math.max(maxY, track.position.y);
             }
+            let w = maxX - minX;
+            let h = maxY - minY;
+            let u = w * 4;
+            let v = h * 4;
+            let wall = BABYLON.MeshBuilder.CreatePlane("wall", { width: h + 0.2, height: w + 0.2, sideOrientation: BABYLON.Mesh.DOUBLESIDE, frontUVs: new BABYLON.Vector4(0, 0, v, u) });
+            wall.position.x = (maxX + minX) * 0.5;
+            wall.position.y = (maxY + minY) * 0.5;
+            wall.position.z += 0.016;
+            wall.rotation.z = Math.PI / 2;
+            wall.material = wallMaterial;
+            let baseFrame = new BABYLON.Mesh("base-frame");
+            baseFrame.position.copyFrom(wall.position);
+            baseFrame.material = this.steelMaterial;
+            this.vertexDataLoader.get("./meshes/base-frame.babylon").then(vertexData => {
+                let positions = [...vertexData[0].positions];
+                for (let i = 0; i < positions.length / 3; i++) {
+                    let x = positions[3 * i];
+                    let y = positions[3 * i + 1];
+                    let z = positions[3 * i + 2];
+                    if (x > 0) {
+                        positions[3 * i] += w * 0.5 - 0.01 + 0.1;
+                    }
+                    else if (x < 0) {
+                        positions[3 * i] -= w * 0.5 - 0.01 + 0.1;
+                    }
+                    if (y > 0) {
+                        positions[3 * i + 1] += h * 0.5 - 0.01 + 0.1;
+                    }
+                    else if (y < 0) {
+                        positions[3 * i + 1] -= h * 0.5 - 0.01 + 0.1;
+                    }
+                }
+                vertexData[0].positions = positions;
+                vertexData[0].applyToMesh(baseFrame);
+            });
         });
     }
     animate() {
